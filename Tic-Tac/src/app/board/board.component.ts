c28fb168f870773c9b1dc2721828fcdffec200f9
Aldane Barrett <barrett.aldane22@gmail.com>
2022-11-27 21:35:59 -0500

    Declare Variable types

--- Tic-Tac/src/app/board/board.component.ts
+++ Tic-Tac/src/app/board/board.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
-export class BoardComponent {
 
+export class BoardComponent implements OnInit{
+  
+  squares: any[];//Used to track the nine different square
+  xIsNext : boolean; //Helps to determine the current player
+  winner : string; //Winner is either 'X' or 'O'
+
+  constructor(){}
+
+  ngOnInit(){}
 }
