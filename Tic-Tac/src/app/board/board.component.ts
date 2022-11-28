5c0e8a53d4b283f508738a88cda00ff82fc718d8
Aldane Barrett <barrett.aldane22@gmail.com>
2022-11-27 22:08:37 -0500

    get Player method

--- Tic-Tac/src/app/board/board.component.ts
+++ Tic-Tac/src/app/board/board.component.ts
@@ -7,12 +7,30 @@ import { Component, OnInit} from '@angular/core';
 })
 
 export class BoardComponent implements OnInit{
-  
+
   squares: any[];//Used to track the nine different square
   xIsNext : boolean; //Helps to determine the current player
   winner : string; //Winner is either 'X' or 'O'
 
   constructor(){}
 
-  ngOnInit(){}
+// Initialize game 
+  ngOnInit(){
+    this.newGame();
+    this.xIsNext = true;
+    this.winner = null;
+  }
+
+// initialize 9 square as null
+  newGame() {
+    this.squares = Array(9).fill(null);
+  }
+
+  // get player
+  get player(){
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+
+  
 }
