8047707d823fe9c9045acce8a312711fc11734bf
Aldane Barrett <barrett.aldane22@gmail.com>
2022-11-29 18:12:48 -0500

    Add nbButton class to square component

--- Tic-Tac/src/app/square/square.component.ts
+++ Tic-Tac/src/app/square/square.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   template: `
-    <p>
-      square works!
-    </p>
-  `,
-  styles: [
-  ]
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+  `, 
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
 
+  @Input() value: 'X' | 'O';
 }
